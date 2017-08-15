33f26dfd9c9ea80af4b5e665b5b022b5efc62b7d
= <bmgdx8@mail.missouri.edu>
2017-08-15 04:11:57 -0700

    feat: Correctly assign .yaml output filename

--- pr2_robot/scripts/project_template.py
+++ pr2_robot/scripts/project_template.py
@@ -301,8 +301,12 @@ def pr2_mover(object_list):
             print "Service call failed: %s"%e
 
     # Output your request parameters into output yaml file
-    # TODO: get actual yaml filename
-    send_to_yaml(pick_place.yaml, dict_list)
+    if test_scene_num.data = 1:
+        send_to_yaml(output_1.yaml, dict_list)
+    else if test_scene_num.data = 2:
+        send_to_yaml(output_2.yaml, dict_list)
+    else if test_scene_num.data = 3:
+        send_to_yaml(output_3.yaml, dict_list)
 
 
 
